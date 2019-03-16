f8cabf48a915c47822424839272370fcba28b388
Ilija Mihajlovic <39649078+IlijaMihajlovic@users.noreply.github.com>
2019-03-16 22:15:39 +0100

    Minor Fixes

---
+++ UIImagePickerControllerProject /UIImagePickerControllerProject/Extension.swift
@@ -0,0 +1,102 @@
+//
+//  Extension.swift
+//  UIImagePickerControllerProject
+//
+//  Created by Ilija Mihajlovic on 3/9/19.
+//  Copyright © 2019 Ilija Mihajlovic. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    
+    func resize(toTargetSize targetSize: CGSize) -> UIImage {
+
+        
+        let newScale = self.scale // change this if you want the output image to have a different scale
+        let originalSize = self.size
+        
+        let widthRatio = targetSize.width / originalSize.width
+        let heightRatio = targetSize.height / originalSize.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        let newSize: CGSize
+        if widthRatio > heightRatio {
+            newSize = CGSize(width: floor(originalSize.width * heightRatio), height: floor(originalSize.height * heightRatio))
+        } else {
+            newSize = CGSize(width: floor(originalSize.width * widthRatio), height: floor(originalSize.height * widthRatio))
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(origin: .zero, size: newSize)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = newScale
+        format.opaque = true
+        let newImage = UIGraphicsImageRenderer(bounds: rect, format: format).image() { _ in
+            self.draw(in: rect)
+        }
+        
+        return newImage
+    }
+}
+
+
+
+public func cropToBounds(image: UIImage, width: Double, height: Double) -> UIImage {
+    
+    let cgimage = image.cgImage!
+    let contextImage: UIImage = UIImage(cgImage: cgimage)
+    let contextSize: CGSize = contextImage.size
+    var posX: CGFloat = 0.0
+    var posY: CGFloat = 0.0
+    var cgwidth: CGFloat = CGFloat(width)
+    var cgheight: CGFloat = CGFloat(height)
+    
+    // See what size is longer and create the center off of that
+    if contextSize.width > contextSize.height {
+        posX = ((contextSize.width - contextSize.height) / 2)
+        posY = 0
+        cgwidth = contextSize.height
+        cgheight = contextSize.height
+    } else {
+        posX = 0
+        posY = ((contextSize.height - contextSize.width) / 2)
+        cgwidth = contextSize.width
+        cgheight = contextSize.width
+    }
+    
+    let rect: CGRect = CGRect(x: posX, y: posY, width: cgwidth, height: cgheight)
+    
+    // Create bitmap image from context using the rect
+    let imageRef: CGImage = cgimage.cropping(to: rect)!
+    
+    // Create a new image based on the imageRef and rotate back to the original orientation
+    let image: UIImage = UIImage(cgImage: imageRef, scale: image.scale, orientation: image.imageOrientation)
+    
+    return image
+}
+
+
+extension UIImageView {
+    var contentClippingRect: CGRect {
+        guard let image = image else { return bounds }
+        guard contentMode == .scaleAspectFit else { return bounds }
+        guard image.size.width > 0 && image.size.height > 0 else { return bounds }
+        
+        let scale: CGFloat
+        if image.size.width > image.size.height {
+            scale = bounds.width / image.size.width
+        } else {
+            scale = bounds.height / image.size.height
+        }
+        
+        let size = CGSize(width: image.size.width * scale, height: image.size.height * scale)
+        let x = (bounds.width - size.width) / 2.0
+        let y = (bounds.height - size.height) / 2.0
+        
+        return CGRect(x: x, y: y, width: size.width, height: size.height)
+    }
+}
